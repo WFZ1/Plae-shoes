d514573ff8f39f60a6d2da393fcb66124cf8ecc1
WFZ1 <p.skumenko@gmail.com>
2020-02-28 19:15:46 +0200

    Image with base64 fixed. The font property compress. Lazy load only visible elements. Fonts types clearing.

--- app/js/script.js
+++ app/js/script.js
@@ -4,42 +4,23 @@
 
 const glideCarousel = new Glide('.featured-products--container');
 
-// Automated height on Carousel build
-glideCarousel.on('build.after', () => glideHandleHeight());
-
-// Automated height on Carousel change
-glideCarousel.on('run.after', () => glideHandleHeight());
-
 // Change number active slide on Carousel change
 let numberActiveSlide = document.querySelector('.featured-products--number span');
 glideCarousel.on(['run'], () => numberActiveSlide.innerHTML = glideCarousel.index + 1 );
 
 // Mount!
 setTimeout(() => {
-	glideCarousel.mount({
-	    type: 'carousel'
-	});
+  glideCarousel.mount({
+    type: 'carousel'
+  });
 }, 500);
 
-// Resize height
-function glideHandleHeight() {
-  const activeSlide = document.querySelector('.glide__slide--active');
-  const activeSlideHeight = activeSlide ? activeSlide.offsetHeight : 0;
-
-  const glideTrack = document.querySelector('.glide__track');
-  const glideTrackHeight = glideTrack ? glideTrack.offsetHeight : 0;
-
-  if (activeSlideHeight !== glideTrackHeight) {
-      glideTrack.style.height = `${activeSlideHeight}px`;
-  }
-}
-
-/* Scroll            
+/* Scroll
 =============================================*/
 
 /*----------  Click to dot  ----------*/
 
-let dotsToScroll = document.querySelectorAll('.hero--dots a');
+let dotsToScroll = document.querySelectorAll('.dots a');
 
 function changeDotClass (item) {
 	dotsToScroll.forEach(el => el.classList.remove('active'));
@@ -65,33 +46,25 @@ for (var i = 0; i < dotsToScroll.length; i++) {
 
 /*----------  Auto dot change  ----------*/
 
-// let sections = document.querySelectorAll('.section'),
-// 		navigationDots = document.querySelectorAll('.hero--dots a');
+let sections = document.querySelectorAll('.section'),
+		navigationDots = document.querySelectorAll('.dots a');
 
-// updateNavigation();
-// window.addEventListener('scroll', () => updateNavigation());
+updateNavigation();
+window.addEventListener('scroll', updateNavigation);
 
-// function updateNavigation() {
-// 	sections.forEach(el => {
-// 		let navDot = document.querySelector('.hero--dots a[href="#' + el.getAttribute('id') + '"]'),
-// 				activeSection = navDot.dataset.number - 1;
+function updateNavigation() {
+	sections.forEach(el => {
+		let navDot = document.querySelector('.dots a[href="#' + el.getAttribute('id') + '"]'),
+				activeSection = navDot.dataset.number - 1;
 
-// 		if ( el.offsetTop < window.pageYOffset + el.offsetHeight / 2 ) {
-// 			navigationDots[activeSection].classList.add('active');
-// 			// console.log('TRUE ' + (el.offsetTop - el.offsetHeight / 2 + ' < ' + window.pageYOffset + ' && ' + el.offsetTop + el.offsetHeight / 2 + ' > ' + window.pageYOffset));
-// 		} else {
-// 			  navigationDots[activeSection].classList.remove('active');
-// 			  // console.log('FALSE ' + (el.offsetTop - el.offsetHeight / 2 + ' < ' + window.pageYOffset + ' && ' + el.offsetTop + el.offsetHeight / 2 + ' > ' + window.pageYOffset));
-// 		}
-// 	});
+    if ( ( el.offsetTop - el.offsetHeight / 2 < window.pageYOffset ) && ( el.offsetTop + el.offsetHeight / 2 > window.pageYOffset ) ) {
+      navigationDots[activeSection].classList.add('active');
+      // console.log('TRUE ' + (el.offsetTop - el.offsetHeight / 2 + ' < ' + window.pageYOffset + ' && ' + el.offsetTop + el.offsetHeight / 2 + ' > ' + window.pageYOffset));
+    } else {
+        navigationDots[activeSection].classList.remove('active');
+        // console.log('FALSE ' + (el.offsetTop - el.offsetHeight / 2 + ' < ' + window.pageYOffset + ' && ' + el.offsetTop + el.offsetHeight / 2 + ' > ' + window.pageYOffset));
+    }
+  });
 
-// 	console.log('');
-
-// 	// ( el.offsetTop - el.offsetHeight / 2 < window.pageYOffset ) && ( el.offsetTop + el.offsetHeight / 2 > window.pageYOffset )
-// }
-
-// easyScrollDots({
-//   'fixedNav': false,
-//   'fixedNavId': '',
-//   'fixedNavUpward': false
-// });
+  // console.log('');
+}
